f75cf58705b77e9a2b725b907d810fe3e42c0047
Son-VT <68632239+Son-VT@users.noreply.github.com>
2020-08-04 18:44:02 +0700

    sửa bài

--- app/src/main/java/com/example/fooddelivery/SignUpActivity.java
+++ app/src/main/java/com/example/fooddelivery/SignUpActivity.java
@@ -1,25 +1,163 @@
 package com.example.fooddelivery;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
     Button btnSignUp;
+    private FirebaseAuth mAuth;
+    TextInputEditText regUser, regPass, regRePass, redSdt;
+    TextInputLayout inputRegUser, inputRegPass, inputRegPass1, inputRegPhone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_activity);
         btnSignUp = findViewById(R.id.btnSignUp);
+        regUser = findViewById(R.id.edtEmail);
+        regPass = findViewById(R.id.edtPassword);
+        regRePass = findViewById(R.id.edtRePassword);
+        inputRegUser = findViewById(R.id.inputEmail);
+        inputRegPass = findViewById(R.id.inputPass);
+        inputRegPass1 = findViewById(R.id.inputRePass);
+        regPass.addTextChangedListener(new ValidationTextWatcher(regPass));
+        regRePass.addTextChangedListener(new ValidationTextWatcher(regRePass));
+        regUser.addTextChangedListener(new ValidationTextWatcher(regUser));
+        mAuth = FirebaseAuth.getInstance();
+
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i  = new Intent(SignUpActivity.this, LoginActivity.class);
-                startActivity(i);
+                if(validateEmail() & validatePassword() == true){
+                    final String email= regUser.getText().toString();
+                    final String password = regPass.getText().toString();
+                    mAuth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Log.v(email,password);
+                                        Toast.makeText(SignUpActivity.this, "Đăng kí thành công",
+                                                Toast.LENGTH_SHORT).show();
+                                        Intent i = new Intent(SignUpActivity.this,LoginActivity.class);
+                                        startActivity(i);
+
+                                    } else {
+                                        Log.v(email,password);
+                                        Toast.makeText(SignUpActivity.this, "Nhập đúng định dạng email, mật khẩu 6 kí tự",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+
             }
         });
+    }
+
+    private void requestFocus(View view) {
+        if (view.requestFocus()) {
+            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        }
+    }
+
+    private boolean validatePassword() {
+        if (regPass.getText().toString().trim().isEmpty()) {
+            inputRegPass.setError("Bắt buộc nhập mật khẩu");
+            requestFocus(regPass);
+            return false;
+        }else if(regPass.getText().toString().length() < 6){
+            inputRegPass.setError("Mật khẩu phải là 6 ký tự");
+            requestFocus(regPass);
+            return false;
+        }else if(regPass.getText().toString().length() > 6) {
+            inputRegPass.setError("Mật khẩu không quá 6 ký tự");
+            requestFocus(regPass);
+            return false;
+        }else {
+            inputRegPass.setErrorEnabled(false);
+        }
+        return true;
+    }
+    private  boolean validateRePass(){
+        String pass = regPass.getText().toString();
+        String rePass = regRePass.getText().toString();
+        if (regRePass.getText().toString().trim().isEmpty()) {
+            inputRegPass1.setError("Nhập lại mật khẩu");
+            requestFocus(regRePass);
+            return false;
+        }
+        else if (rePass.equals(pass)){
+            inputRegPass1.setErrorEnabled(false);
+            return true;
+        }else {
+            inputRegPass1.setError("Mật khẩu không khớp");
+            requestFocus(regRePass);
+
+        }
+        return false;
+    }
+
+    private boolean validateEmail() {
+        if (regUser.getText().toString().trim().isEmpty()) {
+            inputRegUser.setError("Bắt buộc nhập mật Email");
+            requestFocus(regUser);
+            return false;
+        } else {
+            String emailId = regUser.getText().toString();
+            Boolean  isValid = android.util.Patterns.EMAIL_ADDRESS.matcher(emailId).matches();
+            if (!isValid) {
+                inputRegUser.setError("Sai định dạng Email, ex: abc@example.com");
+                requestFocus(regUser);
+                return false;
+            } else {
+                inputRegUser.setErrorEnabled(false);
+            }
+        }
+        return true;
+    }
+    private class ValidationTextWatcher implements TextWatcher {
+
+        private View view;
+
+        private ValidationTextWatcher(View view) {
+            this.view = view;
+        }
+
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void afterTextChanged(Editable editable) {
+            switch (view.getId()) {
+                case R.id.edtEmail:
+                    validateEmail();
+                    break;
+                case R.id.edtPassword:
+                    validatePassword();
+                    break;
+                case R.id.edtRePassword:
+                    validateRePass();
+                    break;
+            }
+        }
     }
 }
